8cfdb059555b2c23ad98bff83b555b3ced4c4cfc
GRAND <bengrand.pro@gmail.com>
2020-04-20 00:21:57 +0200

    add style!

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './Counter.css'
 
 function Counter() {
   // Déclare une nouvelle variable d'état, qu’on va appeler « count »
@@ -7,14 +8,25 @@ function Counter() {
 
   return (
     <div>
+        <h1>Try me!</h1>
+        <input
+        name="try me!"
+        type="number"
+        />
       <p>Le compteur est  à : {count} </p>
+      
       <button onClick={() => setCount(count + 1)}>
         +1
       </button>
       <button onClick={() => setCount(count - 1)}>
         -1
       </button>
-      <input>{useState.count}</input>
+      <br></br>
+      <img 
+        src= 'https://i.pinimg.com/originals/0c/6c/31/0c6c319447cea2759446e6f2266a69af.png'
+        className= 'perceval'
+        alt='Perceval'
+        />
     </div>
   );
 }
